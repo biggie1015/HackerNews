4494f3eeae620c152fada657dfb99e515208ea54
ismel Santana <isantana@oriontek.do>
2022-08-06 01:37:47 -0400

    Adding images in the dropdown

--- HackerNewsApp/src/app/app.component.ts
+++ HackerNewsApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Post } from './model/Post';
 import { HackerNewService } from './services/HackerNewsService';
-
+import { NgSelectModule, NgOption } from '@ng-select/ng-select';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +12,13 @@ export class AppComponent {
   public posts: Array<any> = [];
   filter: string = '';
   page: number = 0;
+  selectedFramework: string = 'angular';
 
+  framework = [
+    { id: 'reactjs', name: 'ReactJs', img: '../assets/images/react.png' },
+    { id: 'angular', name: 'Angular', img: '../assets/images/angular.png' },
+    { id: 'vuejs', name: 'VueJs', img: '../assets/images/vuejs.png' },
+  ];
   constructor(private service: HackerNewService) {
     this.GetList();
   }
--- HackerNewsApp/src/app/app.module.ts
+++ HackerNewsApp/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { NgSelectModule } from '@ng-select/ng-select';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, NgSelectModule],
   providers: [],
   bootstrap: [AppComponent],
 })
